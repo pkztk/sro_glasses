4051b70973743a9cb004710b7e94dc198c35bb9c
pkztk <pkoziatek@gmail.com>
2014-07-24 12:52:26 +0200

    methods.py updated

--- methods.py
+++ methods.py
@@ -1,35 +1,52 @@
-#!/usr/bin/python2
-
-import sys, os, glob
-
+import sys, os, glob, time
 
 def fileLength(file):
     return sum(1 for line in file)
 
 
-def getCoordinates(file):
-
-    at_coords = {}
-
+def reformatFile(file):
     for i in range(fileLength(file)):
-
         if i > 1 and file[i].split()[0] == "0":
-            at_coords[int(file[i].split()[0]) + 10000 + i - 1] = map(float, file[i].split()[1:4])
-
+            open("tmp", "a").write("{} {}\n".format(10000 + i - 1, ' '.join(file[i].split()[1:4])))
         elif file[i].split()[0] == "1":
-            at_coords[int(file[i].split()[0]) + 10000 - i] = map(float, file[i].split()[1:4])
+            open("tmp", "a").write("{} {}\n".format(10000 - i, ' '.join(file[i].split()[1:4])))
 
+
+def distance(a, b, L):
+    dummy = 0
+    for i in range(3):
+        u = a[i] - b[i]
+        if u > L/2:
+            u -= L
+        elif u < -L/2:
+            u += L
+        dummy += u*u
+    return dummy ** 0.5
+
+
+def getCoordinates(file):
+    at_coords = {}
+    reformatFile(file)
+    for line in open("tmp", "r").readlines():
+        at_coords[int(line.split()[0])] = map(float, line.split()[1:4])
     return at_coords
+
 
 def getNeighbors(file):
     
-    at_neighbors = []
+    at_neighbors = {}
+    coords = getCoordinates(file)
 
-    
+    os.system("voro++ -p -c \"%i %n \" -{0} {0} -{0} {0} -{0} {0} tmp".format(float(file[1].split()[1]) / 2))
+
+    fp = open("tmp.vol", 'r').readlines()
+    for line in fp:
+        central = line.split()[0]
+        at_neighbors[int(central)] = []
+        for atom in line.split()[1:]:
+            if distance(coords.get(int(central)), coords.get(int(atom)), float(file[1].split()[1])) < 2.0:
+                at_neighbors[int(central)].append(int(atom))
+    return at_neighbors
 
 
-for file in glob.glob('*.xyz'):
-    dupa
-    f = open(file, "r").readlines()
-    print getCoordinates(f)
 
