7ae89b93cd157805d115af80c5a2011193321a3e
pkztk <pkoziatek@gmail.com>
2014-07-26 18:37:28 +0200

    main routines

--- main.py
+++ main.py
@@ -3,33 +3,33 @@
 import glob, methods
 
 def one(file):
-    print "Each atom nearest neighbors:"
+    print "Each atom nearest neighbors - density {:.2f} g/cm^3:".format(methods.density(file))
     neighbors_call = methods.getNeighbors(file)
     for key, value in neighbors_call.items():
         print "{}\t{}".format(key, value)
 
 
 def two(file):
-    print "Coordination numbers:"
+    print "Coordination numbers - density {:.2f} g/cm^3:".format(methods.density(file))
     coord_number_Si = methods.coordNumber_Si(file)
     for key, value in coord_number_Si[0].items():
         print "{}-fold: {}%".format(key, 100 * float(value) / coord_number_Si[1])
 
 
 def three(file):
-    print "Bond lengths:"
+    print "Bond lengths - density {:.2f} g/cm^3:".format(methods.density(file))
     bonds = methods.bondLength(file)
     print "Si-O: {:.2f}\nO-O: {:.2f}\nSi-Si: {:.2f}".format(*bonds)
 
 
 def four(file):
-    print "Angles:"
+    print "Angles - density {:.2f} g/cm^3:".format(methods.density(file))
     angle = methods.angles(file)
     print "O-Si-O: {:.2f}\nSi-O-Si: {:.2f}".format(*angle)
 
 
 def five(file):
-    print "Ring distribution in % (total quantity in brackets):"
+    print "Ring distribution in % (total quantity in brackets) - density {:.2f} g/cm^3:".format(methods.density(file))
     rings = methods.rings(file)
     for key, value in rings[0].items():
         print "{}-membered: {:.2f}% ({})".format(key, 100 * float(value) / rings[1], value)
